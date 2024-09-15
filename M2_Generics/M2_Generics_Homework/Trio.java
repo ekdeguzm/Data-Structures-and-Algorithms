831ab960c2d8a8fe68ea327afef56450472df9ee
Emil De Guzman <em.deguzm@gmail.com>
2024-09-15 12:48:59 -0700

    create replaceAll method

--- M2_Generics/M2_Generics_Homework/Trio.java
+++ M2_Generics/M2_Generics_Homework/Trio.java
@@ -10,8 +10,8 @@ public class Trio<T> {
 
     }
 
-    public Trio(T itemGroup) {
-        this(item1, item2, item3);
+    public Trio(T item) {
+        this(item, item, item);
     }
 
     public T getItem1() {
@@ -37,4 +37,21 @@ public class Trio<T> {
     public void setItem3(T item3) {
         this.item3 = item3;
     }
+
+    public void replaceAll(T item) {
+        this.item1 = item;
+        this.item2 = item;
+        this.item3 = item;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Trio:\n" +
+                        "  Item 1: %s\n" +
+                        "  Item 2: %s\n" +
+                        "  Item 3: %s",
+                item1, item2, item3);
+    }
+
 }
