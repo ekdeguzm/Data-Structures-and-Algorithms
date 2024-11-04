b3e42483c25cd72746850cc741f91a01972bd4be
Emil De Guzman <em.deguzm@gmail.com>
2024-11-03 16:10:02 -0800

    create sorting methods

--- M9_Sorting_and_Searching/SearchSortHomeworkFiles/Node.java
+++ M9_Sorting_and_Searching/SearchSortHomeworkFiles/Node.java
@@ -1,17 +1,17 @@
 public class Node<T> {
-	
+
 	public T data;
 	public Node<T> next;
 
 	public Node(T dataPortion) {
 		data = dataPortion;
 		next = null;
-	} 
+	}
 
 	public Node(T dataPortion, Node<T> nextNode) {
 		data = dataPortion;
 		next = nextNode;
-	} 
+	}
 
 	public T getData() {
 		return data;
@@ -19,13 +19,16 @@ public class Node<T> {
 
 	public void setData(T newData) {
 		data = newData;
-	} 
+	}
 
 	public Node<T> getNextNode() {
 		return next;
-	} 
+	}
 
 	public void setNextNode(Node<T> nextNode) {
 		next = nextNode;
-	} 
-} 
+	}
+}
+
+
+
